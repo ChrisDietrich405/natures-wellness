7399d936c0baf52040247b35b67a06d5701430f7
Chris <chrisdietrich3666@gmail.com>
2023-02-17 10:59:42 -0500

    added resource page

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 // const test = "https://warbeasts-api.herokuapp.com";
-const test = 'http://localhost:3001/';
+export const test = 'http://localhost:3001/';
 
 export const api = axios.create({
     baseURL: test, // //process.env.NEXT_PUBLIC_CADASTRO_API_HOST
-    
 });
---
+++ pages/resources/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { api, test } from "../api/api";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Container } from "@mui/system";
+
+const Resources = () => {
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    const fetchResource = async () => {
+      const response = await api.get(`/resources`);
+      const { data } = response;
+      console.log(data);
+      setResources(data);
+    };
+    fetchResource();
+  }, []);
+  return (
+    <Container style={{ marginTop: "100px" }}>
+      <Grid container spacing={2}>
+        {resources.map((resource, index) => {
+          return (
+            <>
+              <Grid item xs={3} gap="20px">
+                <Card>
+                  <h2>{resource.name}</h2>
+                  <Image
+                    src={test + resource.image}
+                    height="300"
+                    width="300"
+                    alt={resource.name}
+                  />
+                </Card>
+              </Grid>
+            </>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Resources;
