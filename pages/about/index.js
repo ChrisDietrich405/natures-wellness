195aea70df0b678c135a9b817544732e43a92925
Chris <chrisdietrich3666@gmail.com>
2023-09-24 13:38:41 -0400

    correcting styling

--- pages/about/index.js
+++ pages/about/index.js
@@ -17,104 +17,123 @@ const About = () => {
 
       <section id="section">
         <div className={styles.title_container}>
-          <h2 className={styles.about_intro_sub_title}>
-            I help men and women lose weight and prevent or reverse chronic
-            diseases in a sustainable and enjoyable manner by teaching the power
-            of cultivating healthy habits related to eating more plants and
-            embracing healthy lifestyle practices.
-          </h2>
-        </div>
-        <div className={styles.about_intro}>
-          <Image
-            src={EmPicture}
-            width={300}
-            height={300}
-            alt="Em photo"
-            className={styles.photo}
-          ></Image>
+          <div className={styles.inner_container}>
+            <h2 className={styles.about_intro_sub_title}>
+              I help men and women lose weight and prevent or reverse chronic
+              diseases in a sustainable and enjoyable manner by teaching the
+              power of cultivating healthy habits related to eating more plants
+              and embracing healthy lifestyle practices.
+            </h2>
+            <Card
+              sx={{ mt: 3, px: 10, height: "500px" }}
+              className={styles.content_paragraph}
+            >
+              {" "}
+              <div className={styles.about_intro}>
+                <Image
+                  src={EmPicture}
+                  width={300}
+                  height={300}
+                  alt="Em photo"
+                  className={styles.photo}
+                ></Image>
 
-          <div className={styles.about_intro_text}>
-            <p>
-              I have an immense passion for helping guide others toward a more
-              healthful and vibrant life. This passion has led me to pursue a
-              career as a Registered Dietitian, to obtain a Masters Degree in
-              Nutrition, and to learn as much as I can about balanced nutrition
-              and healthy lifestyle practices.{" "}
-            </p>
-            <p>
-              Early in my career as a Dietitian at several nursing homes in
-              Baltimore City, I witnessed what seemed like immense suffering and
-              reduced quality of life in men and women, some as young as myself
-              at the time (late 20s) due to debilitating diseases like stroke,
-              Type 2 Diabetes, kidney failure, and others.
-            </p>
-            <p>
-              I felt at a loss of how to help them which led me to want to find
-              a way to help prevent others from suffering this same fate.
-              Finding a way to help empower, educate, and support individuals on
-              their journeys to achieving optimal health became my passion and
-              something I feel so grateful to be able to do!
-            </p>
-          </div>
-        </div>
-        <div className={styles.content_text}>
-          <Card sx={{ mt: 3 }} className={styles.content_paragraph}>
-            <p>
-              Eating more plants and engaging in healthy lifestyle practices can
-              help us to look and feel our best! A few of the health problems
-              that can be lessened or resolved a plant-predominant or
-              plant-based way of eating and healthy lifestyle practices
-              includes:
-            </p>
-            <div className={styles.list_container}>
-              <List data={aboutData} />
-            </div>
-          </Card>
-          <Grid container spacing={2} style={{ marginTop: "20px" }}>
-            <Grid item lg={6}>
-              <Card sx={{ mt: 3 }} className={styles.content_paragraph}>
-                <p style={{ marginBottom: "10px" }}>
-                  Stress-management and self care Plant-based nutrition and
-                  purpose Adequate hydration and physical activity Restorative
-                  sleep Connection and community
-                </p>
-                <p style={{ marginBottom: "20px" }}>
-                  In addition to being the most effective, healthy, and
-                  sustainable way to lose weight and maintain weight loss, some
-                  health issues that are effectively addressed by embracing
-                  these practices or doing our best to do so include:
-                </p>
-                <div className={styles.list_container}>
-                  <List data={aboutData3} className={styles.ordered_list} />
+                <div className={styles.about_intro_text}>
+                  <p>
+                    I have an immense passion for helping guide others toward a
+                    more healthful and vibrant life. This passion has led me to
+                    pursue a career as a Registered Dietitian, to obtain a
+                    Masters Degree in Nutrition, and to learn as much as I can
+                    about balanced nutrition and healthy lifestyle practices.{" "}
+                  </p>
+                  <p>
+                    Early in my career as a Dietitian at several nursing homes
+                    in Baltimore City, I witnessed what seemed like immense
+                    suffering and reduced quality of life in men and women, some
+                    as young as myself at the time (late 20s) due to
+                    debilitating diseases like stroke, Type 2 Diabetes, kidney
+                    failure, and others.
+                  </p>
+                  <p>
+                    I felt at a loss of how to help them which led me to want to
+                    find a way to help prevent others from suffering this same
+                    fate. Finding a way to help empower, educate, and support
+                    individuals on their journeys to achieving optimal health
+                    became my passion and something I feel so grateful to be
+                    able to do!
+                  </p>
                 </div>
-              </Card>
-            </Grid>
-            <Grid item lg={6}>
+              </div>
+            </Card>
+
+            <div className={styles.content_text}>
               <Card
-                sx={{ mt: 3, py: 4, px: 10, height: "600px" }}
+                sx={{ mt: 3, py: 4, px: 10, height: "300px" }}
                 className={styles.content_paragraph}
               >
-                <p style={{ marginBottom: "10px" }}>
-                  I believe that all individuals have the right to live a full
-                  and vibrant life. I feel that the foundation for living our
-                  best lives is our health. Though our genes play a role in our
-                  health, their role is small compared to the role that
-                  healthful lifestyle practices have on our physical and mental
-                  wellbeing.{" "}
-                </p>
-                <p style={{ marginBottom: "20px" }}>
-                  {" "}
-                  Healthy lifestyle practices that have a positive impact on our
-                  physical and mental wellbeing include the following. I denote
-                  them as SPARC to help us ignite these practices in our life to
-                  achieve our optional physical and mental wellbeing:{" "}
+                <p>
+                  Eating more plants and engaging in healthy lifestyle practices
+                  can help us to look and feel our best! A few of the health
+                  problems that can be lessened or resolved a plant-predominant
+                  or plant-based way of eating and healthy lifestyle practices
+                  includes:
                 </p>
                 <div className={styles.list_container}>
-                  <List data={aboutData2} />
+                  <List data={aboutData} />
                 </div>
               </Card>
-            </Grid>
-          </Grid>
+              <Grid container spacing={2} style={{ marginTop: "20px" }}>
+                <Grid item lg={6}>
+                  <Card
+                    sx={{ mt: 3, py: 4, px: 10, height: "600px" }}
+                    className={styles.content_paragraph}
+                  >
+                    <p style={{ marginBottom: "10px" }}>
+                      Stress-management and self care Plant-based nutrition and
+                      purpose Adequate hydration and physical activity
+                      Restorative sleep Connection and community
+                    </p>
+                    <p style={{ marginBottom: "20px" }}>
+                      In addition to being the most effective, healthy, and
+                      sustainable way to lose weight and maintain weight loss,
+                      some health issues that are effectively addressed by
+                      embracing these practices or doing our best to do so
+                      include:
+                    </p>
+                    <div className={styles.list_container}>
+                      <List data={aboutData3} className={styles.ordered_list} />
+                    </div>
+                  </Card>
+                </Grid>
+                <Grid item lg={6}>
+                  <Card
+                    sx={{ mt: 3, py: 4, px: 10, height: "600px" }}
+                    className={styles.content_paragraph}
+                  >
+                    <p style={{ marginBottom: "10px" }}>
+                      I believe that all individuals have the right to live a
+                      full and vibrant life. I feel that the foundation for
+                      living our best lives is our health. Though our genes play
+                      a role in our health, their role is small compared to the
+                      role that healthful lifestyle practices have on our
+                      physical and mental wellbeing.{" "}
+                    </p>
+                    <p style={{ marginBottom: "20px" }}>
+                      {" "}
+                      Healthy lifestyle practices that have a positive impact on
+                      our physical and mental wellbeing include the following. I
+                      denote them as SPARC to help us ignite these practices in
+                      our life to achieve our optional physical and mental
+                      wellbeing:{" "}
+                    </p>
+                    <div className={styles.list_container}>
+                      <List data={aboutData2} />
+                    </div>
+                  </Card>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
         </div>
       </section>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Slideshow from "../src/components/Slideshow";
-import styles from "../src/styles/Home.module.css";
 import { homeData } from "../src/data/homeData";
 import List from "../src/components/List";
+
+import styles from "../src/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,47 +14,47 @@ export default function Home() {
           <h2 className={styles.home_subtitle}>Are you:</h2>
 
           <List data={homeData} unordered className={styles.unordered_list} />
+          <p>
+            If you answered yes to any of these questions, you are not alone.
+            Having worked with hundreds of men and women trying to lose weight
+            and improve their health, I have witnessed the immense frustration
+            and despair faced by those who, just like you, often have tried many
+            different ways to lose weight and improve their health but have yet
+            to find a way that is sustainable, inexpensive, and enjoyable. This
+            is not a personal failure but rather one of our society-our diet
+            culture, food industry, ease of access to inexpensive and processed
+            foods, and the abundance of conflicting nutrition and health
+            information do not make it easy.
+          </p>
+          <p style={{ marginTop: "20px" }}>
+            I am here to help you to navigate the challenges that society has
+            thrown on you by working with you to create and cultivate habits
+            that can transform your health and happiness. I am here so that your
+            dreams of losing weight and keeping it off, of having your clothes
+            fit more loosely, for jumping out of bed in the morning with little
+            or no stiffness, for abundant energy, reducing or eliminating your
+            need for some or all medications, and more will become realities
+            long-term.
+          </p>
+          <p className={styles.freeGuide}>
+            Looking to lose weight & improve your health without sacrificing
+            taste, feeling hungry, cutting portion sizes, or counting calories?
+            Grab this free Sensational Six Guide to help you slim down while
+            feeling satisfied and energized! Complete with servings to aim for,
+            ideas on how to boost your intake of plant powerhouses, and links to
+            some delicious recipes!
+          </p>
+          <div className={styles.freeGuideContainer}>
+            <a
+              href="https://drive.google.com/file/d/1aLAgxayEAow11BpVgFSO_LKzoDXxdZ82/view?usp=sharing"
+              className={styles.freeGuideLink}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Get a Free Guide Today!
+            </a>
+          </div>
         </div>
-        <p>
-          If you answered yes to any of these questions, you are not alone.
-          Having worked with hundreds of men and women trying to lose weight and
-          improve their health, I have witnessed the immense frustration and
-          despair faced by those who, just like you, often have tried many
-          different ways to lose weight and improve their health but have yet to
-          find a way that is sustainable, inexpensive, and enjoyable. This is
-          not a personal failure but rather one of our society-our diet culture,
-          food industry, ease of access to inexpensive and processed foods, and
-          the abundance of conflicting nutrition and health information do not
-          make it easy.
-        </p>
-        <p style={{ marginTop: "20px" }}>
-          I am here to help you to navigate the challenges that society has
-          thrown on you by working with you to create and cultivate habits that
-          can transform your health and happiness. I am here so that your dreams
-          of losing weight and keeping it off, of having your clothes fit more
-          loosely, for jumping out of bed in the morning with little or no
-          stiffness, for abundant energy, reducing or eliminating your need for
-          some or all medications, and more will become realities long-term.
-        </p>
-        <p className={styles.freeGuide}>
-          Looking to lose weight & improve your health without sacrificing
-          taste, feeling hungry, cutting portion sizes, or counting calories?
-          Grab this free Sensational Six Guide to help you slim down while
-          feeling satisfied and energized! Complete with servings to aim for,
-          ideas on how to boost your intake of plant powerhouses, and links to
-          some delicious recipes!
-        </p>
-        <div className={styles.freeGuideContainer}>
-          <a
-            href="https://drive.google.com/file/d/1aLAgxayEAow11BpVgFSO_LKzoDXxdZ82/view?usp=sharing"
-            className={styles.freeGuideLink}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Get a Free Guide Today!
-          </a>
-        </div>
-     
       </section>
     </>
   );
