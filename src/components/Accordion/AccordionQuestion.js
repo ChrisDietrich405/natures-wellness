099fc2d83a1b2b26462fb46af6f79d207c95d417
Chris <chrisdietrich3666@gmail.com>
2023-10-09 07:31:43 -0400

    added pics

--- src/components/Accordion/AccordionQuestion.js
+++ src/components/Accordion/AccordionQuestion.js
@@ -19,6 +19,38 @@ const AccordionQuestion = ({ question }) => {
 export default AccordionQuestion;
 
 
-image:"/content/happycow.png"
+// image:"/content/happycow.png"
 
-image:"/content/theveganstay.png"
+// image:"/content/theveganstay.png"
+
+
+// _id
+// 6522f7247f254f5217d2fc8e
+// id
+// 61
+// title
+// "Vegan Stay"
+// url
+// "https://www.theveganstay.com"
+// description
+// null
+// image
+// "/content/theveganstay.png"
+// resource_id
+// 10
+
+
+// _id
+// 6522f42b7f254f5217d2fc8b
+// id
+// 58
+// title
+// "Happy Cow"
+// url
+// "https://www.happycow.net/"
+// description
+// null
+// image
+// "/content/happycow.png"
+// resource_id
+// 10
