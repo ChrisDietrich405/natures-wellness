30f829b9b04976be27f805a1f25d47f3787825f1
Chris <chrisdietrich3666@gmail.com>
2023-10-23 22:05:40 -0400

    test

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "/api";
 
 export const api = axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
