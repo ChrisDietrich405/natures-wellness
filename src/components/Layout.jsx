fb45cb22fa56feae79e7a39f8f7ee75e28ad730f
Flávio Severiano <severiano.flavio18@gmail.com>
2024-01-04 09:39:48 -0300

    refactor(Layout): add padding-top to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import styles from './layout.module.css';
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={styles.main__wrapper}>{children}</main>
       <Footer style={{ position: "fixed", bottom: "0px" }} />
     </>
   );
