93f4e8f278fde2a36da19c3a818f98cf31404c28
Chris <chrisdietrich3666@gmail.com>
2024-02-01 17:56:39 -0500

    refactor testimonials

--- __tests__/testimonials.test.js
+++ __tests__/testimonials.test.js
@@ -1,9 +1,6 @@
 import testimonials from "../pages/api/testimonials";
-import clientPromise from "../src/config/db";
 
-const { client } = jest.requireActual(
-  "../__mocks__/mockMongoDB.js"
-);
+const { client } = jest.requireActual("../__mocks__/mockMongoDB.js");
 
 const jsonMock = jest.fn();
 
@@ -16,47 +13,59 @@ const req = {
   method: "GET",
 };
 
+const exampleTestimonials = [
+  {
+    _id: 1,
+    id: 1,
+    name: "~ Frank B",
+    active: 1,
+    testimonial:
+      "The 8 week weight loss and wellness program with Emily has helped tran…",
+    city: "Ellicott City",
+  },
+  {
+    _id: 2,
+    id: 2,
+    name: "~ Flavio",
+    active: 1,
+    testimonial: "Eu sou o melhor",
+    city: "Ellicott",
+  },
+];
+
 describe("testimonials", () => {
   beforeAll(async () => {
-    const exampleTestimonials = [
-      {
-        _id: 1,
-        id: 1,
-        name: "~ Frank B",
-        active: 1,
-        testimonial:
-          "The 8 week weight loss and wellness program with Emily has helped tran…",
-        city: "Ellicott City",
-      },
-      {
-        _id: 2,
-        id: 2,
-        name: "~ Flavio",
-        active: 1,
-        testimonial: "Eu sou o melhor",
-        city: "Ellicott",
-      },
-    ];
-
     const db = client.db("emily-website-next");
     const collection = db.collection("testimonials");
 
     await collection.insertMany(exampleTestimonials);
   });
 
+  it.only("should return with a status code of 200", async () => {
+    await testimonials({ method: "GET" }, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(jsonMock).toHaveBeenCalledWith(exampleTestimonials);
+  });
+
   it("should return with a status code of 405", async () => {
     await testimonials({ method: "PUT" }, res);
     expect(res.status).toHaveBeenCalledWith(405);
   });
-  it.only("should respond with an error because no collection was found", async () => {
-    const client = await clientPromise
-    client.mockImplementationOnce(() => {
-      throw new Error("no collection found")
-    });
+
+  it("should respond with an error because no collection was found", async () => {
+    jest.mock("../src/config/db.js", () => ({}));
+
+    // const client = await clientPromise;
+    // console.log("before", client);
+
+    // jest.spyOn(await clientPromise, "s");
+    // console.log(client);
+    // client.mockImplementationOnce(() => {
+    //   throw new Error("no collection found");
+    // });
     // const db = clientMock.db("emily-website-next");
 
     // const collectionSpy = jest.spyOn(db, "collection");
-    
 
     // collectionSpy.mockImplementationOnce(() => {
     //   throw new Error("Testimonials not found");
--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,13 @@
 const { client } = jest.requireActual("./__mocks__/mockMongoDB.js");
 
+// ./src/config/db.js
+
 jest.mock("./src/config/db.js", () => {
   const { clientPromise } = jest.requireActual("./__mocks__/mockMongoDB.js");
   return clientPromise;
 });
+
+// ./__mocks__/mockMongoDB.js => ./src/config/db.js
 
 afterAll(async () => {
   await client?.close();
