06e98090ad1ffee91e00ebfba154d5e17db46de3
Flávio Severiano <severiano.flavio18@gmail.com>
2024-02-01 20:29:29 -0300

    refactor(testimonials): remove console.log

--- pages/api/testimonials.js
+++ pages/api/testimonials.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 
     res.status(200).json(testimonials);
   } catch (error) {
-    console.log("ERROR", error);
+
     return res.status(500).json({ status: 500, message: error });
   }
 }
