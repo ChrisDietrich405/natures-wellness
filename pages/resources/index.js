ea0870bc66065f0600e3b534a708ccc3ed5bd9a8
Chris <chrisdietrich3666@gmail.com>
2024-04-18 13:25:33 -0400

    test

--- pages/resources/index.js
+++ pages/resources/index.js
@@ -76,7 +76,7 @@ const Resources = () => {
                       mt: "10px",
                     }}
                   >
-                    Learn More stuff
+                    Learn More stuff tathan
                   </Button>
                 </CardActions>
               </Card>
